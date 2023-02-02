7bcfdf9acbc55cb55027f4796812087edfd0ebba
Pellegrin69 <pellegrinpierre@hotmail.fr>
2023-02-02 11:14:40 +0100

    crud atelier et inscription

--- src/Form/AtelierType.php
+++ src/Form/AtelierType.php
@@ -3,6 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Atelier;
+use App\Entity\Salle;
+use App\Entity\Secteur;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,8 +17,15 @@ class AtelierType extends AbstractType
         $builder
             ->add('titre')
             ->add('ressource')
-            ->add('salle')
-            ->add('secteurs')
+            ->add('salle',EntityType::class,[
+                "class" => Salle::class,
+                "choice_label" => "nom"
+            ])
+            ->add('secteurs',EntityType::class,[
+                "class" => Secteur::class,
+                "choice_label" => "nom",
+                "multiple" => "true"
+            ])
         ;
     }
 
--- src/Form/InscriptionType.php
+++ src/Form/InscriptionType.php
@@ -3,6 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Inscription;
+use App\Entity\Lyceen;
+use App\Entity\Atelier;
+use App\Entity\Creneau;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,9 +17,18 @@ class InscriptionType extends AbstractType
     {
         $builder
             ->add('date')
-            ->add('lyceen')
-            ->add('atelier')
-            ->add('creneau')
+            ->add('lyceen',EntityType::class,[
+                "class" => Lyceen::class,
+                "choice_label" => "nom"
+            ])
+            ->add('atelier',EntityType::class,[
+                "class" => Atelier::class,
+                "choice_label" => "titre"
+            ])
+            ->add('creneau',EntityType::class,[
+                "class" => Creneau::class,
+                "choice_label" => "id"
+            ])
         ;
     }
 
